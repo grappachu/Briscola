b4cbc0e99b5eac0bf8e1ec5f7d22f7242c49d884
Gabriele Suerz <gabriele.suerz@beantech.it>
2020-07-29 19:38:49 +0200

    started gabry404

---
+++ src/Grappachu,Briscola.Strategies.Test/suerzg/Gabry404StrategyTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grappachu.Briscola.Model;
+using Grappachu.Briscola.Players.suerzg;
+using Grappachu.Briscola.Strategies.Test.prenassid;
+using SharpTestsEx;
+using Xunit;
+
+namespace Grappachu.Briscola.Strategies.Test.suerzg
+{
+	public class Gabry404StrategyTest
+	{
+		
+		private readonly Gabry404Strategy _gabry404Strategy;
+
+		public Gabry404StrategyTest()
+		{
+			_gabry404Strategy = new Gabry404Strategy();
+		}
+
+		[Fact(DisplayName = "Alla prima mano se c'è una carta con grandi punti in mano viene giocata ma non se è di briscola")]
+		public void PrimaManoSpecialGabry404()
+		{
+			var briscola = new Card("Spade", 3);
+			var state = CreateDish.CreateHand(_gabry404Strategy,  briscola, new Card[] { },
+				new[]
+				{
+					new Card("Bastoni", 3),
+					new Card("Danari", 2),
+					new Card("Spade", 4)
+				}, 0);
+			var me = state.Players.Single(x => x.Strategy == _gabry404Strategy);
+
+			var card = _gabry404Strategy.Choose(me, state);
+
+			card.Should().Be.EqualTo(new Card("Bastoni", 3));
+		}
+
+		[Fact(DisplayName = "Alla prima mano se c'è una carta con grandi punti in mano viene giocata ma non se è di briscola")]
+		public void PrimaManoSpecialGabry404Not()
+		{
+			var briscola = new Card("Spade", 3);
+			var state = CreateDish.CreateHand(_gabry404Strategy,  briscola, new Card[] { },
+				new[]
+				{
+					new Card("Spade", 3),
+					new Card("Danari", 2),
+					new Card("Spade", 4)
+				}, 0);
+			var me = state.Players.Single(x => x.Strategy == _gabry404Strategy);
+
+			var card = _gabry404Strategy.Choose(me, state);
+
+			card.Should().Not.Be.EqualTo(new Card("Spade", 3));
+		}
+
+		[Fact(DisplayName = "Deve giocare liscio")]
+		public void DeveGiocareLiscio()
+		{
+			var briscola = new Card("Spade", 3);
+			var state = CreateDish.CreateHand(_gabry404Strategy, briscola, new Card[] { },
+				new[]
+				{
+					new Card("Bastoni", 3),
+					new Card("Danari", 2),
+					new Card("Spade", 4)
+				});
+			var me = state.Players.Single(x => x.Strategy == _gabry404Strategy);
+
+			var card = _gabry404Strategy.Choose(me, state);
+
+			card.Should().Be.EqualTo(new Card("Danari", 2));
+		}
+
+		[Fact(DisplayName = "Deve giocare la briscola più bassa in mano se ci sono punti nel piatto")]
+		public void DeveGiocareBriscolaBassaConPuntiNelPiatto()
+		{
+			var briscola = new Card("Spade", 3);
+			var state = CreateDish.CreateHand(_gabry404Strategy, briscola, new Card[]
+				{
+					new Card("Danari", 3)
+				},
+				new[]
+				{
+					new Card("Spade", 7),
+					new Card("Danari", 2),
+					new Card("Spade", 4)
+				});
+			var me = state.Players.Single(x => x.Strategy == _gabry404Strategy);
+
+			var card = _gabry404Strategy.Choose(me, state);
+
+			card.Should().Be.EqualTo(new Card("Spade", 4));
+		}
+
+		[Fact(DisplayName = "Deve giocare tre di briscola se l'asso di briscola è già stato giocato in un'altra mano")]
+		public void DeveGiocareAssoConAssoGiocatoAltraMano()
+		{
+
+		}
+
+	}
+}
---
+++ src/Grappachu.Briscola.Players/suerzg/Gabry404Strategy.cs
@@ -0,0 +1,80 @@
+﻿using Grappachu.Briscola.Interfaces;
+using Grappachu.Briscola.Model;
+using Grappachu.Briscola.Players.Default;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Grappachu.Briscola.Players.suerzg
+{
+    public class Gabry404Strategy : StrategyBase
+    {
+        public const string StrategyName = "gabry404";
+
+        public Dictionary<string, List<Card>> playedCards;
+
+        public Gabry404Strategy() : base(StrategyName)
+        {
+        }
+
+        protected override Card OnChoose(IPlayer myself, GameState state)
+        {
+            if (state.Dish.Count > 0)
+            {
+                Card currentHighest = state.Dish[0];
+                for (var index = 1; index < state.Dish.Count; index++)
+                {
+                    var c = state.Dish[index];
+
+                    if (c.Seed == currentHighest.Seed && c.Value > currentHighest.Value)
+                    {
+                        currentHighest = c;
+                    }
+                    else if (c.Seed == state.Briscola.Seed)
+                    {
+                        currentHighest = c;
+                    }
+                }
+            }
+            else if (state.Turn == 0 && playedCards.Count == 0 &&
+                     myself.HandCards.Any(c => c.Seed != state.Briscola.Seed && (c.Value == 3 || c.Value == 1)))
+            {
+                return myself.HandCards.First(c => c.Seed != state.Briscola.Seed && (c.Value == 3 || c.Value == 1));
+            }
+            else
+            {
+                if (myself.HandCards.Any(c =>
+                    c.Seed != state.Briscola.Seed && c.Value != 3 && c.Value != 1 && c.Value < 8))
+                {
+                    return myself.HandCards.First(c =>
+                        c.Seed != state.Briscola.Seed && c.Value != 3 && c.Value != 1 && c.Value < 8);
+                }
+                else if (myself.HandCards.Any(c =>
+                    c.Seed != state.Briscola.Seed && c.Value != 3 && c.Value != 1 && c.Value > 8))
+                {
+                    return myself.HandCards.First(c =>
+                        c.Seed != state.Briscola.Seed && c.Value != 3 && c.Value != 1 && c.Value > 8);
+                }
+                else
+                {
+                    return myself.HandCards.First();
+                }
+            }
+
+            return myself.HandCards.First();
+        }
+
+        protected override void OnWatch(IPlayer myself, GameState state)
+        {
+            foreach (var card in state.Dish)
+            {
+                if (!playedCards.ContainsKey(card.Seed))
+                {
+                    playedCards[card.Seed] = new List<Card>();
+                }
+
+                playedCards[card.Seed].Add(card);
+                playedCards[card.Seed] = playedCards[card.Seed].OrderBy(point => point.Value).ToList();
+            }
+        }
+    }
+}
